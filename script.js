b1e5f0161b4ba626e76932cba78c4d93323194fc
yishnu96 <yishnupramanik96@gmail.com>
2020-06-25 10:31:11 +0530

    remaining

--- script.js
+++ script.js
@@ -10,17 +10,13 @@ function initialiseBars(){
     }
 }
 
-
 //Smooth Scroll
 for(let i=0; i < navMenuAnchorTags.length;i++){
     navMenuAnchorTags[i].addEventListener('click',function(event){
         event.preventDefault();
         const targetSelectionID = this.textContent.trim().toLowerCase();
-        //console.log(targetSelectionID);
         const targetSelection = document.getElementById(targetSelectionID);
-        // console.log(targetSelection);
         const targetSelectionCoodinates = targetSelection.getBoundingClientRect();
-        // console.log(targetSelectionCoodinates);
         const interval = setInterval(function(){
             const targetSelectionCoodinates = targetSelection.getBoundingClientRect();
             if(targetSelectionCoodinates.top<=0){
@@ -37,7 +33,6 @@ function checkScroll(){
     const coodinates = skillContiner.getBoundingClientRect();
     if(!animationDone && coodinates.top <= window.innerHeight){
         animationDone = true;
-        console.log('Skill Section Visiaable');
         fillBars();
     }
     else if(coodinates.top > window.innerHeight){
@@ -48,9 +43,7 @@ function checkScroll(){
 
 //fills Bars of Skills
 function fillBars(){
-
     for(let bar of progessBars){
-        
         let targetWidth = bar.getAttribute('data-bar-width');
         let currentWidth = 0;
         let interval = setInterval(function(){
